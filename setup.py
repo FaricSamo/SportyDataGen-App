e3f79b3f71e52386076134f807f7a09f2e9cc36d
Samo Faric <samofaric99@gmail.com>
2021-07-16 17:04:20 +0200

    Added requirements.txt file and package sport-activities-features

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='SportyDataGen',
     version='1.0',
     long_description=__doc__,
-    packages=find_packages(),
+    packages=find_packages(include=['sport-activities-features']),
     include_package_data=True,
     zip_safe=False,
     install_requires=['Flask']
